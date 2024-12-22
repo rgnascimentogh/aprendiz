28f2660f2128c2f2edcf57e7d14a190c0a891fc1
Ricardo Nascimento <rgnascimentoms@hotmail.com>
2024-12-22 16:55:42 -0300

    modifiquei para que o usuário insira o valor

--- Fibonacci.c
+++ Fibonacci.c
@@ -6,9 +6,9 @@ int main()
 {
     system("chcp 65001>null"); //configuração para UTF-8 NO WINDOWS
     int a = 0, b = 1, c, i, t;
-    //printf("\nDigite o numero de termos: ");
-    //scanf("%d", &t);
-    t = 8;
+    printf("\nDigite o numero de termos: ");
+    scanf("%d", &t);
+    //t = 8; //modifiquei para o usuário inserir o valor que quiser
     for ( i = 0; i < t; i++)
     {
         c = a + b;
